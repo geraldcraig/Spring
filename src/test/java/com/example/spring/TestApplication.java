81adc93b4dd4433ce3af33e3460d1aa22308748c
geraldcraig <geraldcraig@me.com>
2026-03-08 19:52:52 +0000

    completed

--- src/test/java/com/example/spring/TestApplication.java
+++ src/test/java/com/example/spring/TestApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 public class TestApplication {
 
     public static void main(String[] args) {
-        SpringApplication.from(Application::main).with(TestcontainersConfiguration.class).run(args);
+        SpringApplication.from(AccessingDataJpaApplication::main).with(TestcontainersConfiguration.class).run(args);
     }
 
 }
